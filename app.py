fda17a5fde05f6fd5f812ea9caa37dd241cbf3ab
DESKTOP-L719UGN\Sujitha Arumugam <suji.arumugam@gmail.com>
2022-05-27 08:35:43 +0530

    new

---
+++ app.py
@@ -0,0 +1,12 @@
+
+from flask import Flask, request, render_template, send_from_directory
+from flask_cors import CORS
+
+app = Flask(__name__, static_folder='static', template_folder='static')
+CORS(app)
+app.config['UPLOAD_EXTENSIONS'] = ['.csv', '.xlsx', '.xls', '.json', '.xml']
+app.config['MAX_CONTENT_LENGTH'] = 10000* 1024 * 1024
+
+@app.route("/")
+def helloWorld():
+  return render_template('index.html')
