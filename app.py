c46dbbc8e808b0040037f792b77715803d77b77f
DESKTOP-L719UGN\Sujitha Arumugam <suji.arumugam@gmail.com>
2022-05-27 08:42:44 +0530

    test3

--- app.py
+++ app.py
@@ -10,3 +10,9 @@ app.config['MAX_CONTENT_LENGTH'] = 10000* 1024 * 1024
 @app.route("/")
 def helloWorld():
   return render_template('index.html')
+
+@app.route("/static/")
+def helloWorld1():
+  return render_template('index.html')
+
+
